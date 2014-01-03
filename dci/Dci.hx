3fe1cf987298b340b75ce8d447e0f98e9d4f760b
Andreas <ciscoheat@gmail.com>
2014-01-03 06:11:50 +0100

    Removed most of the static stuff. Added autocompletion for RoleMethods.

--- dci/Dci.hx
+++ dci/Dci.hx
@@ -1,47 +1,72 @@
 package dci;
+import haxe.crypto.Adler32;
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
 using haxe.macro.ExprTools;
 
-#if macro	
-private typedef RoleMap = Map<String, Array<String>>;
+#if macro
+// A map for detecting role names. Role => [RoleMethod, ...]
+private typedef RoleNameMap = Map<String, Array<String>>;
+
+// A map for the RoleMethods, used when adding 'self'.
+private typedef RoleMap = Map<String, Array<Function>>;
+
+// A map for the types of the RoleMethod functions.
+private typedef RoleInterfaceList = Map<String, Array<Field>>;
+
+// The final RoleInterface type, an anonymous type with or without an extension.
+private typedef RoleInterfaces = Map<String, ComplexType>;
 
 class Dci
 {
 	@macro public static function context() : Array<Field>
 	{
-		var fields : Array<Field> = Context.getBuildFields();
-		var hasRole = function(m) { return m.name == "role"; };
-		
+		return new Dci().execute();
+	}
+
+	private static var SELF = "self";
+	private static var ROLEINTERFACE = "roleInterface";
+
+	var roleFields : Array<Field>;
+	var nonRoleFields : Array<Field>;
+	var roleMethodNames : RoleNameMap;
+	var roleMethods : RoleMap;
+	var roleInterfaceList : RoleInterfaceList;
+	var roleInterfaces : RoleInterfaces;
+	
+	public function new()
+	{
+		roleFields = [];
+		nonRoleFields = [];
+		roleMethodNames = new RoleNameMap();
+		roleMethods = new RoleMap();
+		roleInterfaceList = new RoleInterfaceList();
+		roleInterfaces = new RoleInterfaces();
+	}
+	
+	public function execute() : Array<Field>
+	{
 		//trace("Context: " + Context.getLocalClass());
-		
-		var roleFields = [];
-		var nonRoleFields = [];
-		var roleMethods = new RoleMap();
-		
-		// First pass: Add Roles
-		for (field in fields)
-		{
-			//if (Lambda.exists(field.meta, function(m) { return m.name == "dump"; } )) trace(field);
 			
-			if (Lambda.exists(field.meta, hasRole))
-				addRole(field, roleFields);
-			else
-				nonRoleFields.push(field);
-		}
-		
-		// Second pass: Add Role methods
+		var fields = Context.getBuildFields();
+
+		// First pass: Add Role methods and create the RoleInterfaces map.
 		for (field in fields)
 		{
-			if (Lambda.exists(field.meta, hasRole))
-				addRoleMethods(field, roleFields, roleMethods);
+			addRoleMethods(field);
+		}
+
+		// Second pass: Add Roles.
+		for (field in fields)
+		{
+			if(hasRole(field)) addRole(field);
 		}
 		
 		// Third pass: Replace function calls with Role method calls, where appropriate		
 		for (field in fields)
 		{
-			var isRole = Lambda.exists(field.meta, hasRole);
+			var isRole = hasRole(field);
 			
 			var ex : Expr;
 			switch(field.kind)
@@ -52,15 +77,32 @@ class Dci
 			}
 			
 			if (ex != null)
-				ex.iter(function(e) { replaceRoleMethodCalls(e, roleMethods, isRole ? field.name : null); });
+				ex.iter(function(e) { replaceRoleMethodCalls(e, isRole ? field.name : null); } );
+				
+			if (!isRole)
+				nonRoleFields.push(field);
+		}
+		
+		for (roleName in roleMethods.keys())
+		{
+			for (roleMethod in roleMethods[roleName])
+			{
+				addSelfToMethod(roleMethod, roleName, roleMethod.expr.pos);
+			}
 		}
 		
 		return roleFields.concat(nonRoleFields);
 	}
-	
+
+	static function hasRole(field : Field)
+	{
+		return Lambda.exists(field.meta, function(m) { return m.name == "role"; } );
+	}
+
 	// Replace Role method calls with the transformed version.
 	// TODO: It's optimized for speed, but could make it a bit nicer.
-	private static function replaceRoleMethodCalls(e : Expr, roleMethods : RoleMap, roleName : String)
+	// TODO: Detect role method reference and 'self' reference, and act accordingly.
+	function replaceRoleMethodCalls(e : Expr, roleName : String)
 	{
 		switch(e.expr)
 		{
@@ -83,21 +125,21 @@ class Dci
 						// Test if we're past the end of array (no need to check last field),
 						// or if a Role is matching the field.
 						var field = fieldArray[i];
-						if (i > length-2 || !roleMethods.exists(field) || !Lambda.has(roleMethods[field], fieldArray[i + 1]))
+						if (i > length-2 || !roleMethodNames.exists(field) || !Lambda.has(roleMethodNames[field], fieldArray[i + 1]))
 						{
 							newArray.push(field);
 							continue;
 						}
 						
-						var roleMethod = roleMethodName(field, fieldArray[i + 1]);
-						//trace("Role method call: " + roleMethod + " at " + e.pos);
-						newArray.push(roleMethod);					
+						var roleMethod = roleMethodName(field, fieldArray[i + 1]);						
+						newArray.push(roleMethod);
 						skip = true; // Skip next field since it's now a part of the Role method call.
 					}
 					
 					if (fieldArray.length != newArray.length)
 					{
-						e.expr = buildField(newArray, newArray.length-1, e.pos);
+						//trace("Role method call: " + newArray.join(".") + " at " + e.pos);
+						e.expr = buildField(newArray, newArray.length - 1, e.pos);
 						return;
 					}					
 				}
@@ -105,12 +147,12 @@ class Dci
 			case _: 
 		}
 		
-		e.iter(function(e) { replaceRoleMethodCalls(e, roleMethods, roleName); });
+		e.iter(function(e) { replaceRoleMethodCalls(e, roleName); });
 	}
 
 	// Extract the field to an array. this.test.length = ['this', 'test', 'length']
 	// Also replace "self" with the roleName if set.
-	private static function extractField(e : Expr, roleName : String)
+	static function extractField(e : Expr, roleName : String)
 	{
 		var output = [];
 		while (true)
@@ -118,7 +160,7 @@ class Dci
 			switch(e.expr)
 			{
 				case EField(e2, field):
-					var replace = (roleName != null && field == 'self') ? roleName : field;
+					var replace = (roleName != null && field == SELF) ? roleName : field;
 					output.unshift(replace);
 					e = e2;
 				
@@ -126,7 +168,7 @@ class Dci
 					switch(c)
 					{
 						case CIdent(s):
-							var replace = (roleName != null && s == 'self') ? roleName : s;
+							var replace = (roleName != null && s == SELF) ? roleName : s;
 							output.unshift(replace);
 							//trace(output);
 							return output;
@@ -142,7 +184,7 @@ class Dci
 	}
 	
 	// The reverse of extractField.
-	private static function buildField(identifiers, i, pos)
+	static function buildField(identifiers, i, pos)
 	{
 		if (i > 0)
 			return EField({expr: buildField(identifiers, i - 1, pos), pos: pos}, identifiers[i]);
@@ -150,13 +192,38 @@ class Dci
 			return EConst(CIdent(identifiers[i]));
 	}
 	
-	private static function addRole(field : Field, output : Array<Field>)
+	// Special trick for autocompletion: At runtime, only objects that fulfill the RoleInterface
+	// should be bound to a Role. When compiling however, it is convenient to also have the RoleMethods
+	// displayed. Therefore, test if we're in autocomplete mode, add the RoleMethods if so.
+	function mergeTypeAndRoleInterface(fieldName : String, type : TypePath) : ComplexType
 	{
-		if (field.name.toLowerCase() == 'self')
-			Context.error('A Role cannot have the name "self", it is used as an accessor within RoleMethods.', field.pos);
+		if (Context.defined("display") && roleInterfaceList.exists(fieldName))
+		{
+			return TExtend(type, roleInterfaceList[fieldName]);
+		}
+
+		return TPath(type);
+	}
+
+	// Same trick here as above.
+	function mergeAnonymousInterfaces(fieldName : String, fields : Array<Field>) : ComplexType
+	{
+		if (Context.defined("display") && roleInterfaceList.exists(fieldName))
+		{
+			return TAnonymous(fields.concat(roleInterfaceList[fieldName]));
+		}
+
+		return TAnonymous(fields);
+	}
+	
+	//function mergedRoleInterface(fieldName : String
+
+	function addRole(field : Field)
+	{
+		if (field.name == SELF)
+			Context.error('A Role cannot be named "$SELF", it is used as an accessor within RoleMethods.', field.pos);
 		
-		var errorMsg = "Incorrect Role definition: Must be a static var.";
-		var error = function(p) { Context.error(errorMsg, p); };
+		var error = function(p) { Context.error("Incorrect Role definition: Must be a var.", p); };
 		
 		switch(field.kind)
 		{
@@ -169,7 +236,9 @@ class Dci
 					{
 						case TPath(p): 
 							//trace("Adding Role " + field.name + " with only a type");
-							output.push(contextField(FVar(TPath(p)), field.name, [APrivate], field.pos));
+							var fieldType = mergeTypeAndRoleInterface(field.name, p);
+							roleFields.push(contextField(FVar(fieldType), field.name, [APrivate], field.pos));
+							roleInterfaces[field.name] = fieldType;
 							return;
 						case _: error(field.pos);
 					}
@@ -178,7 +247,7 @@ class Dci
 				{
 					switch(e.expr)
 					{
-						// This role is defined using a block, so search for a RoleInterface.
+						// The role is defined using a block, so search for a RoleInterface.
 						case EBlock(exprs):
 							for (expr in exprs)
 							{
@@ -187,19 +256,22 @@ class Dci
 									case EVars(vars):
 										for (v in vars)
 										{
-											if (v.name.toLowerCase() == "roleinterface")
+											if (v.name == ROLEINTERFACE)
 											{
 												switch(v.type)
 												{
 													case TAnonymous(fields):
 														//trace("Adding Role " + field.name + " with RoleInterface");
-														output.push(contextField(FVar(TAnonymous(fields)), field.name, [APrivate], expr.pos));
+														var fieldType = mergeAnonymousInterfaces(field.name, fields);
+														roleFields.push(contextField(FVar(fieldType), field.name, [APrivate], expr.pos));
+														roleInterfaces[field.name] = fieldType;
 														return;
 														
 													case TPath(p):
-														// Simple RoleInterface, just a type.
 														//trace("Adding Role: " + field.name + " with simple RoleInterface");
-														output.push(contextField(FVar(TPath(p), null), field.name, [APrivate], expr.pos));
+														var fieldType = mergeTypeAndRoleInterface(field.name, p);
+														roleFields.push(contextField(FVar(fieldType), field.name, [APrivate], expr.pos));
+														roleInterfaces[field.name] = fieldType;
 														return;
 														
 													case _: Context.error("RoleInterfaces must be defined as a Type or with class notation according to http://haxe.org/manual/struct#class-notation", expr.pos);
@@ -211,9 +283,8 @@ class Dci
 								}
 							}
 							
-							// No RoleInterface found, add the field as Dynamic.
-							//trace("Adding Role " + field.name + " as Dynamic");
-							output.push(contextField(FVar(TPath( { name: 'Dynamic', pack: [], params: [] } ), null), field.name, [APrivate], e.pos));
+							//trace("No RoleInterface found, adding Role " + field.name + " as Dynamic");
+							roleFields.push(contextField(FVar(TPath( { name: 'Dynamic', pack: [], params: [] } ), null), field.name, [APrivate], e.pos));
 							return;
 							
 						case _: error(e.pos);
@@ -245,7 +316,7 @@ class Dci
 		return role + "__" + method;
 	}
 	
-	private static function addRoleMethods(field : Field, output : Array<Field>, methods : RoleMap)
+	function addSelfToMethods(field : Field)
 	{
 		var errorMsg = "Incorrect Role method definition: Must be a block or a Type.";
 		var error = function(p) { Context.error(errorMsg, p); };
@@ -263,16 +334,7 @@ class Dci
 							switch(expr.expr)
 							{
 								case EFunction(name, f):
-									var methodName = roleMethodName(field.name, name);
-									var noCompletion = { pos: f.expr.pos, params: [], name: ":noCompletion" };
-									//trace("Adding role method: " + methodName);
-									output.push(contextField(FFun(f), methodName, [APrivate], f.expr.pos, [noCompletion]));
-									
-									if (!methods.exists(field.name))
-										methods.set(field.name, []);
-										
-									methods[field.name].push(name);									
-									addSelfToMethod(f, field.name);
+									addSelfToMethod(f, field.name, f.expr.pos);
 									
 								case _:
 							}
@@ -285,13 +347,78 @@ class Dci
 		}		
 	}
 	
-	private static function addSelfToMethod(f : Function, roleName : String)
+	function addRoleMethods(field : Field)
 	{
+		if (!hasRole(field)) return;
+		
+		var errorMsg = "Incorrect Role method definition: Must be a block or a Type.";
+		var error = function(p) { Context.error(errorMsg, p); };
+		
+		switch(field.kind)
+		{
+			case FVar(t, e): 
+				if (t != null) return;
+
+				switch(e.expr)
+				{
+					case EBlock(exprs):
+						for (expr in exprs)
+						{
+							switch(expr.expr)
+							{
+								case EFunction(name, f): 
+									var methodName = roleMethodName(field.name, name);
+									var noCompletion = { pos: f.expr.pos, params: [], name: ":noCompletion" };
+									//trace("Adding role method: " + methodName);
+									roleFields.push(contextField(FFun(f), methodName, [APrivate], f.expr.pos, [noCompletion]));
+																		
+									// Store the RoleMethod for usage in next passes
+									if (!roleMethodNames.exists(field.name))
+									{
+										roleMethodNames.set(field.name, []);
+										roleMethods.set(field.name, []);
+									}
+
+									roleMethodNames[field.name].push(name);
+									roleMethods[field.name].push(f);
+									
+									// Autocompletion can only display when the return type is known, 
+									// so only define the RoleInterface if it is set.
+									if (f.ret != null)
+									{
+										if (!roleInterfaceList.exists(field.name))
+											roleInterfaceList.set(field.name, []);
+										
+										// There cannot be a body for the function because we're only creating a field definition,
+										// so a new definition needs to be created.
+										var functionDef = {
+											ret: f.ret,
+											params: f.params,
+											expr: null,
+											args: f.args
+										}
+										
+										roleInterfaceList[field.name].push(contextField(FFun(functionDef), name, [], f.expr.pos));
+									}
+									
+								case _:
+							}
+						}
+						
+					case _: error(e.pos);
+				}
+					
+			case _: error(field.pos);
+		}					
+	}
+	
+	function addSelfToMethod(f : Function, roleName : String, pos : Position)
+	{
+		var type = roleInterfaces.exists(roleName) ? roleInterfaces[roleName] : null;
+				
 		switch(f.expr.expr)
 		{
-			case EBlock(exprs):
-				exprs.unshift(macro var self = this.$roleName);
-				
+			case EBlock(exprs): exprs.unshift(macro var $SELF = this.$roleName);				
 			case _:
 		}
 	}
